faf0021b49b1126fbd1def24c426000244d1f3a6
mauricio10-bit <mauriciopallares10@gmail.com>
2024-09-18 09:57:04 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import miImagen from './assets/img/logoReact.png'
+function App() {
+
+  return (
+    <>
+      <img src={miImagen} alt="Logo react" />
+      <h1>Hola mundo</h1>
+      <p>Este es mi primer proyecto en React</p>
+    </>
+  )
+}
+
+export default App
